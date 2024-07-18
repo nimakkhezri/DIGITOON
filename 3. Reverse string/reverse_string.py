bf76dbfaf17a1f796efb2ee5e36529b6f3b6b359
Nima Koohkhezri <nimakoohkhezri@gmail.com>
2024-07-18 20:50:17 +0330

    bugs fixed

--- 3. Reverse string/reverse_string.py
+++ 3. Reverse string/reverse_string.py
@@ -1,6 +1,23 @@
-input_string = input("Please enter your \"String\": ")
+input_str = input("Please enter your \"String\": ")
+
+def swap_str(input_str: str, i: int, j: int) -> str:
+    swapped_str = ""
+    iterator = 0
+    str_iterator = len(input_str) - 1
+    while(iterator <= str_iterator) :
+        if iterator == i :
+            swapped_str += input_str[j]
+            iterator += 1
+        elif iterator == j :
+            swapped_str += input_str[i]
+            iterator += 1
+        else :
+            swapped_str += input_str[iterator]
+            iterator += 1
+    return swapped_str
 
 def get_reverse(input_string: str) -> str:
+    reversed_str = input_string
     i = 0
     j = len(input_string) - 1
 
@@ -10,12 +27,12 @@ def get_reverse(input_string: str) -> str:
         elif not input_string[j].isalnum() :
             j -= 1
         else :
-            temp = input_string[j]
-            input_string[j] = input_string[i]
-            input_string[i] = temp
+            reversed_str = swap_str(reversed_str, i, j)
+            i += 1
+            j -= 1
     
-    return input_string
+    return reversed_str
 
-reversed_string = get_reverse(input_string)
+reversed_str = get_reverse(input_str)
 
-print(reversed_string)
+print(reversed_str)
